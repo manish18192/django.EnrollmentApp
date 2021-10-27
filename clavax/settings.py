7ff3d2cb0af5f993de00f40c9aafe274ccd7808f
Manish Kumar <82822858+manish18192@users.noreply.github.com>
2021-10-27 18:40:07 +0530

    Enter your Secret key

--- clavax/settings.py
+++ clavax/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5wqg$7e+%pd+nb26dlul+jrt_2#2h2%yrh0w$z*1d6a#!=%n%d'
+SECRET_KEY = # Enter Your Secret key #
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
